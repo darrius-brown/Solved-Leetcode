220581b4bfd97e2cbeebe416ff78206e04c877c4
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2022-10-21 18:53:09 -0700

    Time: 33 ms (99.38%), Space: 13.9 MB (19.75%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,17 +1,12 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         
-        solution = 0
-        for x in range(len(nums)):
-
-            i = nums[x] 
-            for y in range(1, len(nums)- 1):
-                if nums[y] - i == diff:
-                    j = nums[y]
-                    for z in range(y + 1,len(nums)):
-                        if nums[z] - j == diff:
-                            solution += 1
-
-        return solution
+        ans = 0
+        n = len(nums)
+        for i in range(n):
+            if nums[i] + diff in nums and nums[i] + 2 * diff in nums:
+                ans += 1
+        
+        return ans
             
             
