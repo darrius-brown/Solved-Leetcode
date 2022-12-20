15de87fd2acdf305f11be4e05c8d3bd150c35d3b
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2022-12-19 17:06:11 -0800

    Time: 863 ms (98.48%), Space: 36.1 MB (82.81%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -0,0 +1,18 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        l,m,p,g,res=0,0,0,0,0
+        for i in range(len(garbage)):
+            if 'M' in garbage[i] and m<i:
+                m=i
+            if 'P' in garbage[i] and p<i:
+                p=i
+            if 'G' in garbage[i] and g<i:
+                g=i
+            l+=len(garbage[i])
+        if m!=0:
+            res=sum(travel[:m])
+        if p!=0:
+            res+=sum(travel[:p])
+        if g!=0:
+            res+=sum(travel[:g])
+        return res+l
