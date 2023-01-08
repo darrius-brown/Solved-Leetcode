2f3f3141bd38c76c4d7bfde80660887c656c5237
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-01-08 15:43:12 -0800

    Time: 70 ms (96.34%), Space: 44.6 MB (45.20%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -5,13 +5,11 @@
 var pivotIndex = function(nums) {
     let leftSum = nums[0]
     let rightSum = nums.reduce((partialSum, a) => partialSum + a, 0) - nums[0];
-    console.log('right sum = ', rightSum)
     if (rightSum === 0) {
       return 0
     }
     for (let i = 1; i < nums.length; i++) {
         rightSum -= nums[i]
-        console.log('left ' + leftSum, 'right ' + rightSum)
         if (leftSum == rightSum) {
             return i
         } else {
