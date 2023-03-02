14862648c0048dac82223d809809769d47436d5d
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-03-01 18:00:27 -0800

    Time: 66 ms (90.14%), Space: 44.7 MB (69.59%) - LeetHub

---
+++ font-stylevertical-align-inherit-font-stylevertical-align-inherit-2574-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-left-and-right-sum-differences-font-font/font-stylevertical-align-inherit-font-stylevertical-align-inherit-2574-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-font-fontfont-stylevertical-align-inherit-font-stylevertical-align-inherit-left-and-right-sum-differences-font-font.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRigthDifference = function(nums) {
+    let sumRight = nums.reduce((a,b) => a + b);
+    let sumLeft = 0;
+    const a = nums.slice(0, nums.length - 1);
+    a.unshift(0)
+    
+    for(let i = 0; i < nums.length; i++){
+        sumRight -= nums[i]
+        sumLeft += a[i]
+        a[i] = Math.abs(sumLeft - sumRight)
+    }
+    
+    return a;
+};
