c2b1509e532bac49f5009d4704ca92f4aa18bf16
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-10-24 00:38:56 -0700

    Time: 51 ms (59.16%), Space: 41.5 MB (90.27%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+
+var filter = function(arr, fn) {
+    let filteredArray = [];
+
+   for(let i = 0;i < arr.length;i++){
+       if(fn(arr[i],i)){
+           filteredArray.push(arr[i])
+       }
+   }
+   return filteredArray;
+};
