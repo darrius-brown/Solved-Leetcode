9a1ba2a696c05a0da283eaa2d08a8b766107f86a
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-11-25 14:47:44 -0800

    Time: 57 ms (39.39%), Space: 41.8 MB (72.94%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.js
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.js
@@ -5,26 +5,15 @@
  */
 var kthFactor = function(n, k) {
     output = []
-    
-    if(n === 1) {
-        return 1
-    }
-    
-    for (i = 1; i <= Math.floor(n/2); i++) {
-        if (n % i === 0 && output.includes(i) === true) {
-            break
-        }
+  
+    for (i = 1; i <= n; i++) {  
         if (n % i === 0) {
             output.push(i)
-            if(output.includes(n/i) === false)
-            output.push(n/i)
         }
     }
-    sortedOutput = output.sort(function(a, b){return a-b})
     if (output.length < k) {
         return -1
     } else {
-        console.log(sortedOutput)
-    return sortedOutput[k - 1]
+    return output[k - 1]
     }
 };
