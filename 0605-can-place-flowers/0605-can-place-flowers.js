23dbb7fda34398b470eeb353b97b3e2b79b843ce
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-11-26 20:02:29 -0800

    Time: 31 ms (100.00%), Space: 46.3 MB (9.39%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,47 +4,16 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-    let current
-    let previous
-    let next = flowerbed[1]
-    potsleft = n
-    
-    if (flowerbed.length === 1 && flowerbed[0] === 0) {
-        return true
+    flowerbed = [0,...flowerbed,0]
+    for(let i = 1; i < flowerbed.length -1 ; i ++){
+        if(flowerbed[i -1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 ){
+                flowerbed[i] = 1
+                n--
+        }
+        if(n <= 0) return true
+        
     }
+
+     return n <= 0
     
-    if (n === 0) {
-        return true
-    }
-    
-    for (i = 0; i < flowerbed.length; i++) {
-        
-        current = flowerbed[i]
-        
-        if (i === 0 && current === 0 && next === 0) {
-            potsleft--
-            current = 1
-            console.log('case1')
-        }
-        
-        if (current === 0 && previous === 0 && next === 0) {
-            potsleft--
-            current = 1
-            console.log('case2')
-        }
-        
-        if (i === (flowerbed.length - 2) && current === 0 && next === 0) {
-            potsleft--
-            current = 1 
-            console.log('case3')
-        }
-        
-        if (potsleft === 0) {
-            return true
-        }
-        
-        previous = current
-        next = flowerbed[i + 2]
-    } 
-    return false
 };
