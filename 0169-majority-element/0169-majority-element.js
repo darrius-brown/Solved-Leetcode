499092733e6f4caf04efc2613097d793f3b4ef9f
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-12-03 22:42:59 -0800

    Time: 54 ms (79.89%), Space: 45.4 MB (29.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,26 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
-    output = {}
-    for (const i in nums) {
-        if (output.hasOwnProperty(nums[i])) {
-            output[nums[i]]++
-        } else {
-            output[nums[i]] = 1
-        }
-    }
-    let counts = Object.values(output)
-    highest = 0
-    highestIndex = 0
-    curr = 0
-    for (const i in counts) {
-        curr = counts[i]
-        if (curr > highest) {
-            highest = curr
-            highestIndex = i
-        }
-    }
-    
-    return Object.keys(output)[highestIndex]
-};
+var majorityElement = function (nums) {
+    let half = Math.floor(nums.length / 2);
+    nums.sort();
+    return nums[half];
+}
