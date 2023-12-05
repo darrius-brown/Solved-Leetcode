7adb5e62feed910ed67e30521e58b277e3274aee
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-12-04 19:54:28 -0800

    Time: 73 ms (51.72%), Space: 51.7 MB (44.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,20 +3,18 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    if (prices.length < 2) {
-        return 0;
-    }
+    let profit = 0;
+    let minValue = prices[0];
+    
+    for(let i=1; i<prices.length;i++){
+    	if(prices[i] < minValue){
+            minValue = prices[i];
+		}
 
-    let minPrice = prices[0];
-    let maxProfit = 0;
-
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else {
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+		if((prices[i] - minValue) > profit){
+      	    profit = prices[i] - minValue;
         }
     }
-
-    return maxProfit;
+    
+    return profit;
 };
