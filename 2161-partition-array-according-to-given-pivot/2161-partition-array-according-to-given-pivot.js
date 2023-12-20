33dd6d3c3249791f3efffc0692f898c4e4153d68
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-12-19 21:58:38 -0800

    Time: 309 ms (9.24%), Space: 94.7 MB (44.02%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function(nums, pivot) {
+    let before = []
+    let after = []
+    let piv = []
+    for (const i in nums) {
+        if (nums[i] < pivot) {
+            before.push(nums[i])
+        }
+        if (nums[i] === pivot) {
+            piv.push(nums[i])
+        }
+        if (nums[i] > pivot) {
+            after.push(nums[i])
+        }
+    
+    }
+    return [...before, ...piv, ...after]
+}
