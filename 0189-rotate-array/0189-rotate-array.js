cfdbb5d3319208a856eb267b8e4a62fa757e0146
Darrius Brown <91982228+darrius-brown@users.noreply.github.com>
2023-12-25 00:01:31 -0800

    Time: 86 ms (34.31%), Space: 51.9 MB (52.72%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    var newStart = nums.splice(-k);
+    nums.unshift(...newStart);
+    return nums;
+};
